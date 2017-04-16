0eb484c7ba2ceaddf09717845248886a26b3daec
Takahiro YANO <yano@mma.cs.tsukuba.ac.jp>
2017-04-16 23:05:40 +0900

    cmake configure skelton

---
+++ include/lilypad/info.hpp
@@ -0,0 +1,10 @@
+#ifndef LILYPAD_INFO_HPP
+#define LILYPAD_INFO_HPP
+
+namespace lilypad {
+    extern const char* git_hash;
+    extern const char* config_date;
+    extern const char* version;
+}
+
+#endif // LILYPAD_INFO_HPP
